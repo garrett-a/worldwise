2cd6336dd6e2ac64d4904dbd8be4375c55c29225
Garrett Aldridge <garrettaldridge@Garretts-MBP.attlocal.net>
2024-02-29 22:48:11 -0600

    210

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function PageNotFound() {
+  return (
+    <>
+      <PageNav />
+      <div>PageNotFound</div>
+    </>
+  );
+}
+
+export default PageNotFound;
